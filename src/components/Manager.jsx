a880aaf6806d5ddd91c3e3c397f82619e6682b0c
Abhishek <abhisheksharma270820@gmail.com>
2025-04-09 23:32:01 +0530

    Make changes on UI and also save the passwords on localStorage

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,9 +1,28 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 
 
 const Manager = () => {
 
     const ref = useRef();
+    const [form, setform] = useState({
+        site: "", 
+        username: "",
+        password: ""
+    })
+    const [passwordArray, setpasswordArray] = useState([]);
+
+    useEffect(() => {
+      let password = localStorage.getItem("passwords");
+      if(password) {
+        setpasswordArray(JSON.parse(password))
+      }
+    }, [])
+    
+
+    const handleChange = (e) => {
+      setform({...form, [e.target.name]: e.target.value})
+    }
+    
 
     const showPassword = ()=>{
         alert("show the password");
@@ -13,6 +32,13 @@ const Manager = () => {
             ref.current.src = "icons/eyecross.png";
         }
     }
+
+    const handleSave = () => {
+      setpasswordArray([...passwordArray, form]);
+      localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
+      console.log([...passwordArray, form]);
+    }
+    
     return (
         <div>
             <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
@@ -28,18 +54,18 @@ const Manager = () => {
                 <p className='text-center text-lg text-green-900'>Your own Password Manager</p>
 
                 <div className='text-black flex flex-col p-4 gap-8 items-center'>
-                    <input placeholder='Enter Website URL' className='bg-white rounded-full border border-green-500 p-4 py-1 w-full' type="text" name="" id="" />
+                    <input value={form.site} placeholder='Enter Website URL' onChange={handleChange} className='bg-white rounded-full border border-green-500 p-4 py-1 w-full' type="text" name="site" />
                     <div className='flex w-full gap-8'>
-                        <input placeholder='Enter Username' className='bg-white rounded-full border border-green-500 p-4 py-1 w-full' type="text" name="" id="" />
+                        <input value={form.username} placeholder='Enter Username' onChange={handleChange} className='bg-white rounded-full border border-green-500 p-4 py-1 w-full' type="text" name="username" />
                         <div className='relative'>
 
-                        <input placeholder='Enter Password' className='bg-white rounded-full border border-green-500 p-4 py-1 w-full' type="text" name="" id="" />
-                        <span className='absolute top-[3px] right-[5px] cursor-pointer' onClick={showPassword}>
-                            <img ref={ref} className='p-1' width={26} src="icons/eyecross.png" alt="show" />
+                        <input value={form.password} placeholder='Enter Password' onChange={handleChange} className='bg-white rounded-full border border-green-500 p-4 py-1 w-full' type="text" name="password" />
+                        <span className='absolute top-[3px] right-[5px] w-[30px] h-[30px] cursor-pointer flex justify-center items-center' onClick={showPassword}>
+                            <img ref={ref} className='w-[20px] h-[20px] object-contain' src="icons/eyecross.png" alt="show" />
                         </span>
                         </div>
                     </div>
-                    <button className='flex justify-center items-center bg-green-400 rounded-full gap-2 px-8 py-2 w-fit border border-green-900 hover:bg-green-500'>
+                    <button onClick={handleSave} className='flex justify-center items-center bg-green-400 rounded-full gap-2 px-8 py-2 w-fit border border-green-900 hover:bg-green-500'>
                         <lord-icon
                             src="https://cdn.lordicon.com/jgnvfzqg.json"
                             trigger="hover">
